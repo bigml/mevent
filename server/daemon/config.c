6d89a27a8fb9717f8bb87a8b438d18345c4aabb2
bigml <bigmaliang@gmail.com>
2015-02-12 16:47:09 +0800

    server - print error on config_parse_file() fail

--- server/daemon/config.c
+++ server/daemon/config.c
@@ -17,13 +17,17 @@
 
 int config_parse_file(const char *file, HDF **cfg)
 {
+    bool readok = true;
     NEOERR *err;
 
     err = hdf_init(cfg);
     RETURN_V_NOK(err, 0);
-    
+
     err = hdf_read_file(*cfg, file);
-    RETURN_V_NOK(err, 0);
+    if (err != STATUS_OK) readok = false;
+    OUTPUT_NOK(err);
+
+    if (!readok) return 0;
 
     return 1;
 }
